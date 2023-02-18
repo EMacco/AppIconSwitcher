1c634f21ce9220684333486c770805ea62d888bb
Emmanuel Okwara <emmanuel.okwara@deliveryhero.com>
2023-02-18 19:01:47 +0100

    initial commit

--- AppIconSwitcher/ViewController.swift
+++ AppIconSwitcher/ViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var catImageView: UIImageView!
+  @IBOutlet weak var dogImageView: UIImageView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    let tapCat = UITapGestureRecognizer(target: self, action: #selector(self.didSelectCat))
+    catImageView.addGestureRecognizer(tapCat)
+    
+    let tapDog = UITapGestureRecognizer(target: self, action: #selector(self.didSelectDog))
+    dogImageView.addGestureRecognizer(tapDog)
   }
-
-
+  
+  @objc private func didSelectCat() {
+    guard UIApplication.shared.supportsAlternateIcons else {
+      print("App icon switching not supported")
+      return
+    }
+    UIApplication.shared.setAlternateIconName(nil) { error in
+      if let error = error {
+        print(error.localizedDescription)
+      } else {
+        print("Success!")
+      }
+    }
+  }
+  
+  @objc private func didSelectDog() {
+    guard UIApplication.shared.supportsAlternateIcons else {
+      print("App icon switching not supported")
+      return
+    }
+    UIApplication.shared.setAlternateIconName("AppIcon-2") { error in
+      if let error = error {
+        print(error.localizedDescription)
+      } else {
+        print("Success!")
+      }
+    }
+  }
 }
 
